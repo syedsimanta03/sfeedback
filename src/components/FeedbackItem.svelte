14087bbe0133627f42825bdaf5822efae72239f3
syedsimanta03 <syed.simanta06@gmail.com>
2022-06-27 16:56:52 +0600

    first commit

---
+++ src/components/FeedbackItem.svelte
@@ -0,0 +1,13 @@
+<script>
+import Card from "./Card.svelte";
+
+
+  export let item
+</script>
+
+
+<Card>
+  <div class="num-display">{item.rating}</div>
+  <div class="close">X</div>
+  <div class="text-display">{item.text}</div>
+</Card>
