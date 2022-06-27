0d7b409752b45af48aa76d1bdfd10af0e543b571
syedsimanta03 <syed.simanta06@gmail.com>
2022-06-28 00:47:42 +0600

    witoutStore

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script>
 import FeedbackList from "./components/FeedbackList.svelte";
+import FeedbackStats from "./components/FeedbackStats.svelte";
+import FeedbackForm from "./components/FeedbackForm.svelte";
 
 
   let feedback = [
@@ -19,10 +21,25 @@ import FeedbackList from "./components/FeedbackList.svelte";
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
   },
 ]
+
+$: count = feedback.length
+$: average = feedback.reduce((a, {rating}) => a + rating, 0) /count
+
+const addFeedback = e => {
+  const newFeedback = e.detail
+  feedback = [newFeedback, ...feedback]
+}
+
+const deleteFeedback = e => {
+  const id =  e.detail
+  feedback = feedback.filter(item => item.id != id)
+}
 </script>
 
-<main>
-  <FeedbackList {feedback}/>
+<main class='container'>
+  <FeedbackForm on:add-feedback={addFeedback} />
+  <FeedbackStats {count} {average} />
+  <FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
 </main>
 
 
